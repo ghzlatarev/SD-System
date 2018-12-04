d1d01cf2a696549b11f9540e5ecfc71d8f5b439b
ghz <george.zlatareff@gmail.com>
2018-12-05 01:40:15 +0200

    small fix on JS file

--- SD.Web/SD.Web/wwwroot/js/notifications.js
+++ SD.Web/SD.Web/wwwroot/js/notifications.js
@@ -6,10 +6,14 @@ connection.on("ReceiveNotification", function (message) {
 
 	var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 	var encodedMsg = msg;
+
+	var lis = document.createElement("li");
+	lis.setAttribute('class', "nav-link");
 	var li = document.createElement("nav-item");
 	li.setAttribute('dropdown-item', true)
 	li.textContent = encodedMsg;
-	document.getElementById("notificationsList").appendChild(li);
+	lis.appendChild(li);
+	document.getElementById("notificationsList").appendChild(lis);
 	document.getElementById("notificationsCount").innerHTML = document.getElementById("notificationsList").childElementCount - 1;
 });
 
